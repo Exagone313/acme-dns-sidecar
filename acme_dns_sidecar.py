5ec26a32598f195b816605fa462a64e390249b55
Elouan Martinet <exa@elou.world>
2020-05-23 19:15:46 +0200

    [src] Parse command line arguments

--- acme_dns_sidecar.py
+++ acme_dns_sidecar.py
@@ -1,9 +1,25 @@
-def entrypoint(argv):
-    print(argv)
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+
+def entrypoint():
+    args = get_program_args()
+    print(args)
     return 0
+
+
+def get_program_args():
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '-c',
+        dest='config',
+        default='/etc/acme-dns/config.cfg',
+        metavar='config_file',
+        help='path to configuration file, can use the same file as acme-dns',
+    )
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
     import sys
-    ret = entrypoint(sys.argv)
+    ret = entrypoint()
     sys.exit(ret)
